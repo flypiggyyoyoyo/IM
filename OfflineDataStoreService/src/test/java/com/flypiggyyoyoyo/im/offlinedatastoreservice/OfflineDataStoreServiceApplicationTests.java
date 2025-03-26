1a51bd10f30e48f240591bfdd995ff6883661a70
flypiggy <150204110+flypiggyyoyoyo@users.noreply.github.com>
2025-03-26 17:24:27 +0800

    optimise struct

---
+++ OfflineDataStoreService/src/test/java/com/flypiggyyoyoyo/im/offlinedatastoreservice/OfflineDataStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flypiggyyoyoyo.im.offlinedatastoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflineDataStoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
